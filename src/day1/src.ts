364615a2e0a504f6271fe301cb806cbf70d3ac59
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2024-12-01 06:44:34 +0100

    Day 1

--- src/day1/src.ts
+++ src/day1/src.ts
@@ -1,11 +1,53 @@
 export function part1(input: string) {
-  const result = 0;
+  const parsed = parse(input);
+  const sorted = sort(parsed);
+  const diffs = diff(sorted);
+  const sum = diffs.reduce((acc, curr) => acc + curr);
+
+  console.log(sum);
+}
+
+export function part2(input: string) {
+  const parsed = parse(input);
+  const result = parsed[0]
+    .reduce((acc, curr) => acc + curr * occurrences(curr, parsed[1]), 0);
 
   console.log(result);
 }
 
-export function part2(input: string) {
-  const result = 0;
+type Element = number;
+type List = Element[];
+type Lists = [List, List];
 
-  console.log(result);
+export function parse(input: string): Lists {
+  const left = [];
+  const right = [];
+  for (const row of input.split("\n")) {
+    if (row.length === 0) continue; // Last line
+    const [ l, r ] = row.split(" ").filter(Boolean);
+    left.push(Number(l));
+    right.push(Number(r));
+  }
+  return [ left, right ];
+}
+
+export function sort(lists: Lists): Lists {
+  return [
+    lists[0].toSorted(),
+    lists[1].toSorted(),
+  ];
+}
+
+export function diff(lists: Lists): List {
+  const diffs = [];
+  for (let i = 0; i < lists[0].length; i++) {
+    diffs.push(Math.abs(lists[0][i] - lists[1][i]));
+  }
+  return diffs;
+}
+
+export function occurrences(value: Element, list: List): number {
+  return list
+    .filter((v) => v === value)
+    .reduce((acc) => acc + 1, 0);
 }
--- src/day1/test.ts
+++ src/day1/test.ts
@@ -1,8 +1,43 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
+import { readFileSync } from "node:fs";
+
+import { parse, sort, diff, occurrences } from "./src";
+
+const input = readFileSync(`${import.meta.dirname}/example.txt`, { encoding: "utf8" });
 
 describe("Day1", () => {
-  it("Should be true", () => {
-    assert.strictEqual(true, true);
+  it("parses correctly", () => {
+    assert.deepStrictEqual(parse(input), [
+      [ 3, 4, 2, 1, 3, 3 ],
+      [ 4, 3, 5, 3, 9, 3 ],
+    ]);
+  });
+
+  it("sorts correctlt", () => {
+    const parsed = parse(input);
+    assert.deepStrictEqual(sort(parsed), [
+      [ 1, 2, 3, 3, 3, 4 ],
+      [ 3, 3, 3, 4, 5, 9 ],
+    ]);
+  });
+
+  it("calculates diffs correctly", () => {
+    const parsed = parse(input);
+    const sorted = sort(parsed);
+
+    assert.deepStrictEqual(diff(sorted), [
+      2, 1, 0, 1, 2, 5,
+    ]);
+  });
+
+  it("calculates occurrence correctly", () => {
+    const parsed = parse(input);
+
+    assert.strictEqual(occurrences(parsed[0][0], parsed[1]), 3);
+    assert.strictEqual(occurrences(parsed[0][1], parsed[1]), 1);
+    assert.strictEqual(occurrences(parsed[0][2], parsed[1]), 0);
+    assert.strictEqual(occurrences(parsed[0][4], parsed[1]), 3);
+    assert.strictEqual(occurrences(parsed[0][5], parsed[1]), 3);
   });
 });
