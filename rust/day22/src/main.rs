43e5ab7cfea4835d5f77375eb0d9cae5647c44f2
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2024-12-27 07:07:03 +0100

    Day 22 part 2 in rust

--- rust/day22/src/main.rs
+++ rust/day22/src/main.rs
@@ -1,3 +1,7 @@
+use itertools::Itertools;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::time::Instant;
 
 fn main() {
@@ -22,7 +26,43 @@ fn part1(input: &str) {
     println!("{}", result);
 }
 
-fn part2(input: &str) {}
+fn part2(input: &str) {
+    let mut map: HashMap<u64, i32> = HashMap::new();
+
+    input
+        .lines()
+        .map(|s| s.parse::<u64>().unwrap())
+        .for_each(|secret| {
+            let result: Vec<i32> = (0..2000)
+                .scan(secret, |state, i| {
+                    if i == 0 {
+                        Some(secret)
+                    } else {
+                        *state = evolve(*state);
+                        Some(*state)
+                    }
+                })
+                .map(|digit| (digit % 10) as i32)
+                .collect();
+
+            let mut first_found_map: HashMap<u64, i32> = HashMap::new();
+            for w in result.windows(5) {
+                let diffs: Vec<i32> = w.iter().tuple_windows().map(|(a, b)| b - a).collect();
+                let mut hasher = DefaultHasher::new();
+                diffs.hash(&mut hasher);
+                let key = hasher.finish();
+                let value = w[w.len() - 1];
+                first_found_map.entry(key).or_insert(value);
+            }
+
+            for (key, value) in first_found_map {
+                *map.entry(key).or_insert(0) += value;
+            }
+        });
+
+    let result = map.values().max().unwrap_or(&0);
+    println!("{}", result);
+}
 
 fn mix(value: u64, secret: u64) -> u64 {
     value ^ secret
